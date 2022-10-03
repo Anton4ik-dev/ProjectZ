5c1fe828e79fa24c769f4b22cacd2e07e48b2cc1
Anton4ik-dev <afedot2004@gmail.com>
2022-10-03 14:53:42 +0300

    MusicStart

--- Assets/_source/Canvas/SceneManagement.cs
+++ Assets/_source/Canvas/SceneManagement.cs
@@ -8,11 +8,16 @@ public class SceneManagement : MonoBehaviour
 {
     [SerializeField] private Button _play;
     [SerializeField] private Button _settings;
+    [SerializeField] private Button _back;
     [SerializeField] private Button _exit;
+    [SerializeField] private GameObject _panel1;
+    [SerializeField] private GameObject _panel2;
     private void Start()
     {
         _play.onClick.AddListener(Play);
         _exit.onClick.AddListener(Exit);
+        _settings.onClick.AddListener(Settings);
+        _back.onClick.AddListener(Back);
     }
     private void Play()
     {
@@ -21,5 +26,15 @@ public class SceneManagement : MonoBehaviour
     private void Exit()
     {
         Application.Quit();
+    }
+    private void Settings()
+    {
+        _panel1.SetActive(false);
+        _panel2.SetActive(true);
+    }
+    private void Back()
+    {
+        _panel1.SetActive(true);
+        _panel2.SetActive(false);
     }
 }
---
+++ Assets/_source/Music/AudioSources.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSources : MonoBehaviour
+{
+    public AudioSource _mouse;
+    public AudioSource _knock;
+    public AudioSource _water;
+    public AudioSource _oldMan;
+    public AudioSource _bark;
+    [Space]
+    public AudioSource _main;
+    public AudioSource _buttonSound;
+    public AudioSource _drink;
+    public AudioSource _startAlco;
+    public AudioSource _alcoMusic;
+    public AudioSource _endAlco;
+}
